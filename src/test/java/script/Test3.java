edcbddb38ad9e68ad6244354246f1843a2acf9e7
bhavyashreebc <38578644+bhavyashreebc@users.noreply.github.com>
2022-12-29 22:35:07 +0530

    Create Test3.java

---
+++ src/test/java/script/Test3.java
@@ -0,0 +1,18 @@
+package script;
+
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentTest;
+
+import generic.BaseTest;
+import generic.Utility;
+import page.HomePage;
+
+public class Test3 extends BaseTest
+{
+	
+		
+	}
+}
